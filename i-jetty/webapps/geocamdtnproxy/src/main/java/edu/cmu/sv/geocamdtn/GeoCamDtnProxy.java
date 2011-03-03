4290edbe85649252f7d64f77bde9cbceea351eef
hbarnor <henry DOT barnor AT sv.cmu.edu>
2011-03-03 05:43:30 -0800

    Updates to use MultiPartFilter and providing a valid response to geocamlens.

--- i-jetty/webapps/geocamdtnproxy/src/main/java/edu/cmu/sv/geocamdtn/GeoCamDtnProxy.java
+++ i-jetty/webapps/geocamdtnproxy/src/main/java/edu/cmu/sv/geocamdtn/GeoCamDtnProxy.java
@@ -2,6 +2,8 @@ package edu.cmu.sv.geocamdtn;
 
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -10,7 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.mortbay.servlet.MultiPartRequest;
+import android.util.Log;
+
+// import org.mortbay.servlet.MultiPartRequest;
 
 
 
@@ -20,8 +24,7 @@ import org.mortbay.servlet.MultiPartRequest;
  */
 public class GeoCamDtnProxy extends HttpServlet 
 {
-    String proofOfLife = null;
-    DTNServiceConnector serviceConnector = null;
+    // DTNServiceConnector serviceConnector = null;
     
     /* ------------------------------------------------------------ */
     public void init(ServletConfig config) throws ServletException
@@ -31,39 +34,57 @@ public class GeoCamDtnProxy extends HttpServlet
         Object o = config.getServletContext().getAttribute("org.mortbay.ijetty.contentResolver");
         android.content.ContentResolver resolver = (android.content.ContentResolver)o;
         android.content.Context androidContext = (android.content.Context)config.getServletContext().getAttribute("org.mortbay.ijetty.context");
-        proofOfLife = androidContext.getApplicationInfo().packageName;
         
-        serviceConnector = new DTNServiceConnector(context);
+        // serviceConnector = new DTNServiceConnector(context);
     }
 
     /* ------------------------------------------------------------ */
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-		// Gets the uuid to construct the receipt	
-		MultiPartRequest mPRequest = new MultiPartRequest(request);
-		// String uuid_key = "uuid";
-		// String uuid;
-		// if (mPRequest.contains(uuid_key))
-		//     {
-		// 	uuid = mPRequest.getString(uuid_key);
-		//     }
-    	sendResponse(response, "stuff");
-    	
+	// Just some debug for the params 
+	Map params = request.getParameterMap();
+	Iterator i = params.keySet().iterator();
+	while ( i.hasNext() )
+	    {
+		String key = (String) i.next();
+		String value = ((String[]) params.get( key ))[ 0 ];
+		Log.d("GeoCamDtnProxy", key + " " + value);
+	    }
+
+	String uuidKey = "uuid";
+	String uuid = request.getParameter(uuidKey);
+	String fileNameKey = "photo";
+	String fileName = request.getParameter(fileNameKey);
+	Log.d("GeoCamDtnProxy", "Filename is " + fileName);
+	if (null != fileName)
+	    {
+		sendResponse(response, fileName);
+	    }
+	else 
+	    {
+		sendResponse(response, "");
+	    }
     	// Send bundle
-    	serviceConnector.bindDTNService();
+    	// serviceConnector.bindDTNService();
     	//serviceConnector.send(mPRequest.);
-    	serviceConnector.unbindDTNService();
+    	// serviceConnector.unbindDTNService();
     }
     
     private void sendResponse(HttpServletResponse response, String uuid) throws IOException
     {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        //ServletOutputStream out = response.getOutputStream();
-        //out.println("<html>");
-        // out.println("file posted <!--\nGEOCAM_SHARE_POSTED %s\n-->");
-        // out.println("</html>");
-        // out.flush();
+        response.setContentType("text/html");        
+	String filePosted = "file posted <!--\nGEOCAM_SHARE_POSTED ";
+	String endFilePosted = "\n-->";
+	StringBuffer buf = new StringBuffer();
+	buf.append(filePosted);
+	buf.append(uuid);
+	buf.append(endFilePosted);
+	PrintWriter out = response.getWriter();
+	out.println("<html>");
+	out.println(buf);
+        out.println("</html>");
+        out.flush();
+	out.close();
     }
 
     
