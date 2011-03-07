0d6530260174b546de28ab221ac201df7b2f5b7b
hbarnor <henry DOT barnor AT sv.cmu.edu>
2011-03-06 16:07:32 -0800

    Added top-level pom and created a lib to share constants, etc between proxy and android service.

---
+++ dtn-lib/src/main/java/edu/cmu/sv/geocamdtn/lib/Constants.java
@@ -0,0 +1,6 @@
+package edu.cmu.sv.geocamdtn.lib;
+
+public interface Constants {
+	static final String ACTION_CREATE_DTN_BUNDLE = "edu.cmu.sv.geocamdtn.ACTION_CREATE_DTN_BUNDLE"; 
+
+}
--- geocamdtnproxy/src/main/java/edu/cmu/sv/geocamdtn/GeoCamDtnProxy.java
+++ geocamdtnproxy/src/main/java/edu/cmu/sv/geocamdtn/GeoCamDtnProxy.java
@@ -12,20 +12,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
-// import org.mortbay.servlet.MultiPartRequest;
+import edu.cmu.sv.geocamdtn.lib.Constants;
 
 
 
 /* ------------------------------------------------------------ */
 /** GeoCamDtnProxy Servlet 
- * 
+ *  Receives the mime encoded data from geocam and sends it to the
+ * geocam dtn service for dtn transport.
+ * Returns a transmitted receipt to the geocamlens application. 
+ *
  */
 public class GeoCamDtnProxy extends HttpServlet 
 {
-    // DTNServiceConnector serviceConnector = null;
     
+    private static final String TAG = "edu.cmu.sv.geocamdt.ServletProxy";
+
     /* ------------------------------------------------------------ */
     public void init(ServletConfig config) throws ServletException
     {
@@ -34,8 +40,6 @@ public class GeoCamDtnProxy extends HttpServlet
         Object o = config.getServletContext().getAttribute("org.mortbay.ijetty.contentResolver");
         android.content.ContentResolver resolver = (android.content.ContentResolver)o;
         android.content.Context androidContext = (android.content.Context)config.getServletContext().getAttribute("org.mortbay.ijetty.context");
-        
-        // serviceConnector = new DTNServiceConnector(context);
     }
 
     /* ------------------------------------------------------------ */
@@ -43,19 +47,23 @@ public class GeoCamDtnProxy extends HttpServlet
     {
 	// Just some debug for the params 
 	Map params = request.getParameterMap();
-	Iterator i = params.keySet().iterator();
+	// since we will call getParameter, we cannot use the inputstream or the buffered reader
+	// so lets reecreate the mime encoded data and then send it to dtn
+	sendToDTN(params);
+
+ 	Iterator i = params.keySet().iterator();
 	while ( i.hasNext() )
 	    {
 		String key = (String) i.next();
 		String value = ((String[]) params.get( key ))[ 0 ];
-		Log.d("GeoCamDtnProxy", key + " " + value);
+		Log.d(TAG, key + " " + value);
 	    }
 
 	String uuidKey = "uuid";
 	String uuid = request.getParameter(uuidKey);
 	String fileNameKey = "photo";
 	String fileName = request.getParameter(fileNameKey);
-	Log.d("GeoCamDtnProxy", "Filename is " + fileName);
+	Log.d(TAG, "Filename is " + fileName);
 	if (null != fileName)
 	    {
 		sendResponse(response, fileName);
@@ -64,10 +72,6 @@ public class GeoCamDtnProxy extends HttpServlet
 	    {
 		sendResponse(response, "");
 	    }
-    	// Send bundle
-    	// serviceConnector.bindDTNService();
-    	//serviceConnector.send(mPRequest.);
-    	// serviceConnector.unbindDTNService();
     }
     
     private void sendResponse(HttpServletResponse response, String uuid) throws IOException
@@ -87,5 +91,28 @@ public class GeoCamDtnProxy extends HttpServlet
 	out.close();
     }
 
+    /**
+     * Add the parameters to an intent and send it to the
+     * GeoCamDTN service for dtn enqueing.
+     *
+     */
+    private void sendToDTN(Map params)
+    {
+	// lets create a bundle with all that we need
+	Bundle data = new Bundle();
+	Iterator i = params.keySet().iterator();
+	String key;
+	String[] values;
+
+	while ( i.hasNext() )
+	    {
+		key = (String) i.next();
+		values = ((String[]) params.get( key ));
+		data.putStringArray(key, values);
+	    }
+	Intent geoCamDTNIntent = new Intent(Constants.ACTION_CREATE_DTN_BUNDLE);
+
+	
+    }
     
 }
