4fc0592bfa3a7d0ac6884ed67b58eb5be1617928
jiajih <chang.jiajih@gmail.com>
2011-03-13 10:25:33 -0700

    Committing TestIntentGenerator

---
+++ TestIntentGenerator/src/edu/cmu/sv/geocamdtn/test/TestIntentGenerator.java
@@ -0,0 +1,33 @@
+package edu.cmu.sv.geocamdtn.test;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+
+import edu.cmu.sv.geocamdtn.lib.Constants;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class TestIntentGenerator extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    public void sendIntent(View view) {
+		Bundle data = new Bundle();
+
+		data.putStringArray("test", new String[] {"data1", "data2"});
+
+		Intent geoCamDTNIntent = new Intent(Constants.ACTION_CREATE_DTN_BUNDLE);
+		geoCamDTNIntent.putExtra(Constants.IKEY_DTN_BUNDLE_PAYLOAD, data);
+		Log.i("TestIntentGenerator", "Sending intent");
+		this.startService(geoCamDTNIntent);    	
+    }
+}
