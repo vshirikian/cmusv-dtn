e02bd1ec3051de6e274e0c02be6a15bfa6338771
Vache Shirikian <vshirikian@gmail.com>
2011-03-13 12:41:27 -0700

    updated Constants.java with dictionary keys

--- dtn-lib/src/main/java/edu/cmu/sv/geocamdtn/lib/Constants.java
+++ dtn-lib/src/main/java/edu/cmu/sv/geocamdtn/lib/Constants.java
@@ -2,10 +2,17 @@ package edu.cmu.sv.geocamdtn.lib;
 
 public interface Constants {
 	static final String ACTION_CREATE_DTN_BUNDLE = "edu.cmu.sv.geocamdtn.ACTION_CREATE_DTN_BUNDLE";
+	static final String ACTION_MEDIATE_DTN_BUNDLE = "edu.cmu.sv.geocamdtn.ACTION_MEDIATE_DTN_BUNDLE";
+
 	
-	// The fileKey as specified in GeoCam source code.
+	// The file key as specified in GeoCam source code.
 	static final String FILE_KEY = "photo";
 
+	// The keys for mapping the intent sent dictionary (HashMap)
+	static final String DTN_DEST_EID_KEY = "dest_eid";
+	static final String DTN_EXPIRATION_KEY = "expiration";
+	static final String DTN_PAYLOAD_KEY = "payload";
+	
 	
 	// Keys for intent extra fields
 	static final String IKEY_DTN_BUNDLE_PAYLOAD = "edu.cmu.sv.geocamdtn.DTN_BUNDLE_PAYLOAD";
