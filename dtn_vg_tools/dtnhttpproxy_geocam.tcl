46288e54e9422d4bf00f2561ebc68929fbb6b016
Vache <vache@staticgw.(none)>
2011-04-04 06:17:08 -0700

    working staticgw dtn<->http proxy, use with --url <geocam share web app>

--- dtn_vg_tools/dtnhttpproxy_geocam.tcl
+++ dtn_vg_tools/dtnhttpproxy_geocam.tcl
@@ -16,6 +16,7 @@
 #
 
 package require http
+package require mime
 load libdtntcl[info sharedlibextension] dtnapi
 
 #----------------------------------------------------------------------
@@ -62,7 +63,7 @@ proc usage {} {
     puts "    -h | --help        Print help message"
     puts "    -v | --verbose     Verbose mode"
     puts "    -e | --eid <eid>   Set eid (default is dtn://<local_eid>/http)"
-    puts "    -u | --url <url>   Set url (default is http://localhost:8888/upload/"
+    puts "    -u | --url <url>   Set url (default is http://localhost:8888/upload/guest/"
 }
 
 #----------------------------------------------------------------------
@@ -126,8 +127,20 @@ proc register {} {
 #----------------------------------------------------------------------
 proc proxy_loop {} {
     global handle opt DTN_PAYLOAD_FILE
+
+    # use passed in url for http post or default
+    set url $opt(url)
+    if {$url == ""} {
+        set url http://localhost:8888/upload/guest/
+    }
+
+    # http post content-type
+    set content_type "multipart/form-data; boundary=--------multipart_formdata_boundary$--------"
+
+    set i 0
     while {1} {
-        dbg "calling dtn_recv..."
+        set i [expr {$i + 1}]
+        dbg "********\[bundle $i\] waiting on dtn_recv..."
         set bundle [dtn_recv $handle $DTN_PAYLOAD_FILE -1]
         if {$bundle == "NULL"} {
             error "error in dtn_recv: [dtn_strerror [dtn_errno $handle]]"
@@ -148,83 +161,34 @@ proc proxy_loop {} {
         dbg "  payload: $payload_file"
         dbg "  payload_size: [file size $payload_file]"
 
+	# open payload file and read to local variable
+	set fsize [file size $payload_file]
         set fd [open $payload_file r]
+        fconfigure $fd -translation binary
+        set payload_data [read $fd $fsize]
 
-        # default values for params
-        #set params(mode) get
-        set params(mode) upload
-        #set params(content_type) "text/plain"
-        
-        set paramlist [lindex [split $dest ?] 1]
-        foreach param [split $paramlist \;] {
-            foreach {var val} [split $param =] {
-                set params($var) $val
-            }
-        }
-	# vache - hardcode geocam share web server ip for now
-        #if {![info exists params(url)]} {
-        #    puts stderr "no url in destination eid '$dest'"
-        #    continue
-        #}
+        # for no particular reason, write the last bundle to file
+	set lb [open "./last_bundle.txt" w]
+        puts -nonewline $lb $payload_data
 
-        #set url $params(url)
-        #set url [regsub -all "%3a" $url :]
-        #set url [regsub -all "%2f" $url /]
-	set url $opt(url)
-	if {$url == ""} {
-            set url http://localhost:8888/upload/guest/
-	}
+        # for no particular reason, save file with http response
+        set outfd [open "./response.html" w]
+        fconfigure $outfd -translation binary
 
-        switch $params(mode) {
-            upload {
-                # need to wrap the multipart headers around the payload
-                #set temp_file "/tmp/dtnhttpproxy-[pid]"
-                #set tmpfd [open $temp_file w]
-                #if {![info exists params(upload_name)]} {
-                #    set params(upload_name) "file"
-                #}
-                
-                #puts -nonewline $tmpfd "--xYzZY\r\n"
-                #puts -nonewline $tmpfd "Content-Disposition: form-data; name=\"$params(upload_name)\"; filename=\"$temp_file\";\r\n"
-                #puts -nonewline $tmpfd "Content-Type: $params(content_type)\r\n"
-                #puts -nonewline $tmpfd "\r\n"
-                
-                #while {![eof $fd]} {
-                #    set buf [read $fd 1024]
-                #    puts -nonewline $tmpfd $buf
-                #}
-                #puts -nonewline $tmpfd "--xYzZY--\r\n"
-                
-                #close $fd
-                #close $tmpfd
+	#debug
+        #dbg "\n\n****PAYLOAD DATA:\n$payload_data"
 
-                #if [catch {file delete $payload_file} err] {
-                #    puts "error removing payload file: $err"
-                #}
 
-                #set payload_file $temp_file
-                #set fd [open $temp_file]
-                set fd [open $payload_file]
-		# vache - edit for geocam boundry
-                #set params(content_type) "multipart/form-data, boundary=xYzZY"
-                #set params(content_type) "multipart/form-data, boundary=--------multipart_formdata_boundary$--------" 
-                set params(content_type) "multipart/form-data"
-                set channel -querychannel
-            }
-
-            default {
-                puts stderr "mode $params(mode) not defined"
-                continue
-            }
-        }
-        
+        # http post
         if [catch {
             dbg -nonewline " calling http::geturl for $url..."
             set token [::http::geturl $url \
-                    -type $params(content_type) \
-                    -blocksize 1024 \
-                    -progress http_progress \
-                    $channel $fd]
+                      -type $content_type \
+		      -binary true \
+                      -blocksize 1024 \
+                      -progress http_progress \
+                      -channel $outfd \
+                      -query $payload_data]
 
             dbg -nonewline " waiting..."
             ::http::wait $token
@@ -232,18 +196,18 @@ proc proxy_loop {} {
 
             dbg " status: [::http::status $token]"
             dbg " error:  [::http::error $token]"
+	    close $outfd
+
+            # send return receipt bundle
+            set resp [open "./response.html" r]
+            send_receipt "<html>[read $resp]</html>" $source $dest
+            close $resp
             
         } err] {
             puts "error in geturl: $err"
         }
         puts stderr ""
-
-        #if [catch {file delete $payload_file} err] {
-        #    puts "error removing payload file: $err"
-        #}
-	
 	close $fd
-
     }
 }
 
@@ -251,6 +215,209 @@ proc http_progress {token total current} {
    dbg -nonewline .
    flush stdout
 }
+
+proc debug {} {
+    global handle opt DTN_PAYLOAD_FILE
+
+    # use passed in url for http post or default
+    set url $opt(url)
+    if {$url == ""} {
+        set url http://localhost:8888/upload/guest/
+    }
+
+    # http post content-type
+    set content_type "multipart/form-data; boundary=--------multipart_formdata_boundary$--------"
+
+    set fsize [file size "./last_bundle.txt"]
+    set fd [open "./last_bundle.txt" r]
+    fconfigure $fd -translation binary
+    set payload_data [read $fd $fsize]
+    #set payload_data [join [split $payload_data \n]]
+    #set lb [open "./last_bundle.txt" w]
+    #puts -nonewline $lb $payload_data
+    #dbg "\n\n****PAYLOAD DATA:\n$payload_data"
+
+    set outfd [open "./response.html" w]
+    fconfigure $outfd -translation binary
+
+
+    # http post
+    if [catch {
+        dbg -nonewline " calling http::geturl for $url..."
+        set token [::http::geturl $url \
+                  -type $content_type \
+		  -binary true \
+                  -blocksize 1024 \
+                  -progress http_progress \
+                  -channel $outfd \
+                  -query $payload_data]
+
+        dbg -nonewline " waiting..."
+        ::http::wait $token
+        dbg ""
+
+        dbg " status: [::http::status $token]"
+        dbg " error:  [::http::error $token]"
+            
+    } err] {
+        puts "error in geturl: $err"
+    }
+    puts stderr ""
+    close $fd
+}
+
+#----------------------------------------------------------------------
+proc uniq { filename } {
+    # lets grab a number from the file name 
+    # regexp {([:digit:]+)$} $filename idx
+    set ext [file extension $filename]
+    set idx 0
+    set rootfilename [file rootname $filename]
+    set tempname $rootfilename
+    append tempname "-${idx}${ext}"
+    while { [file exists $tempname] } {
+	set tempname $rootfilename
+	incr idx 1
+	append tempname "-${idx}${ext}"
+    }
+
+    return $tempname
+}
+
+#----------------------------------------------------------------------
+proc writeImage {filename data} {
+   if [catch {open $filename w} fileId] {
+	puts stderr "Cannot open $filename : $fileId"
+    } else {
+	fconfigure $fileId -translation binary -encoding binary
+	puts -nonewline $fileId $data
+	flush $fileId
+	close $fileId
+    }
+}
+
+#----------------------------------------------------------------------
+proc saveParts {parts} {
+    set pwd [file dirname [info script]]
+    # lets create the output dir
+    set dir [file join $pwd "out"]
+    file mkdir $dir
+    set idx [expr [lsearch -exact $parts "uuid"] + 1 ]
+    set uuid [lindex $parts $idx]
+    set idx [expr [lsearch -exact $parts "filename"] + 1 ]
+    set fileName [lindex $parts $idx]
+
+    set photoFile [uniq [file join $dir $fileName]]
+    set uuid [file rootname $photoFile]
+
+    #dbg "$uuid"
+    set outputFile ${uuid}.txt
+
+    # dbg "Set outputfile $outputFile"
+    if [catch {open $outputFile w} fileId] {
+	puts stderr "Cannot open $outputFile : $fileId"
+    } else {
+	foreach {key value} $parts {
+	    #dbg "$key $value"
+	    if {[string equal $key "photo"]} {
+		
+		writeImage $photoFile $value
+	    } else {
+		puts $fileId "$key $value"
+	    }
+	}
+	flush $fileId
+	close $fileId
+    }
+}
+
+#----------------------------------------------------------------------
+proc parseMimeValue {value} {
+    set parts [split $value \;]
+    set results [list [string trim [lindex $parts 0]]]
+    set paramList [list]
+    foreach sub [lrange $parts 1 end] {
+	if {[regexp -- {([^=]+)=(.+)} $sub match key val]} {
+            set key [string trim [string tolower $key]]
+            set val [string trim $val]
+            # Allow single as well as double quotes
+            if {[regexp -- {^["']} $val quote]} { ;# need a " for balance
+	       	      if {[regexp -- ^${quote}(\[^$quote\]*)$quote $val x val2]} {
+			  # Trim quotes and any extra crap after close quote
+			  set val $val2
+		      }
+	    }
+		lappend paramList $key $val
+        }
+     }
+     if {[llength $paramList]} {
+		    lappend results $paramList
+     }
+     return $results
+}	
+
+#----------------------------------------------------------------------
+proc flattenMime {message} {
+    set result [list]
+    if {[catch {
+        set children [mime::getproperty $message parts]
+    }]} {
+        # getting parts failed - it's a leaf
+        # in this case, get Content-ID header
+        # and compare it to requested filename
+	# dbg "We have a leaf"
+	# lappend result $content
+        if {![catch {
+            set contentID [mime::getheader \
+			       $message "Content-Disposition"]
+        }]} {
+            # if filename matches Content-ID
+	    set content [mime::getbody $message]
+	    set header [parseMimeValue $contentID]
+	    set key [lindex [lindex $header 1] 1]
+	    # dbg $header
+	    # dbg $key 
+	    lappend result $key $content
+	    if {[string equal $key "photo"]} {
+	    	set fileName [lindex [lindex $header 1] 3]
+	    	dbg "Filename is $fileName"
+	    	lappend result filename $fileName
+	    }
+        }
+    }  else {
+        # for multipart/*, we'll iterate over children
+        # to see if any of them contains the attachment
+	# dbg "Children $children"
+        foreach token $children {
+	    # dbg "token is $token" 
+	    # dbg -nonewline "Body is " 
+	    # dbg [mime::getbody $token]
+            set result [concat $result \
+                [flattenMime $token] \
+                ]
+        }
+    }
+    return $result
+}
+
+#----------------------------------------------------------------------
+proc send_receipt {receipt src dst} {
+    global handle regid DTN_PAYLOAD_MEM COS_NORMAL
+    dbg "\nsending geocam return receipt..."
+    dbg "$receipt\n\n"
+    
+#    tell_dtntest 0 dtn_send $h0 regid=$publisher_0 \
+     #       source=$g0 dest=$g0 expiration=3600 payload_data="bundle1" \
+      #      sequence_id=$seqid01
+
+    set id [dtn_send $handle $regid $dst $src dtn:none $COS_NORMAL 0 86400 $DTN_PAYLOAD_MEM $receipt]
+    # puts "bundle id:"
+    # puts "  source: [dtn_bundle_id_source_get $id]"
+    # puts "  creation_secs: [dtn_bundle_id_creation_secs_get $id]"
+    # puts "  creation_seqno: [dtn_bundle_id_creation_seqno_get $id]"
+    # delete_dtn_bundle_id $id
+}
+
 
 #----------------------------------------------------------------------
 init $argv
@@ -260,10 +427,10 @@ while {1} {
         connect
         register
         proxy_loop
+	#debug
     } err] {
         puts "$err"
     }
     catch {dtn_close $handle} err
-    after 5000
+    after 20000
 }
-
